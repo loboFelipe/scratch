377255945a4df5c9c8b110ea32fb357fbbbebaec
Felipe <felipe@tangle.ca>
2024-10-26 13:37:11 -0600

    drag start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,105 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    type CanvaCodeBlock = {
+        id: string,
+        x: number,
+        y: number
+    }
+
+    let codeBlocks = $state<CanvaCodeBlock[]>([])
+    let selected = $state<CanvaCodeBlock>()
+    let snapshots = $state<CanvaCodeBlock[][]>([])
+    let history = $state(-1)
+    
+    function allowDrop(e: DragEvent) {
+        e.preventDefault();
+        
+        const svgEl = e.target as SVGElement;
+        const { left, top } = svgEl.getBoundingClientRect();
+        const x = +(e.clientX - left).toFixed();
+        const y = +(e.clientY - top).toFixed();
+    }
+
+    function drop(e: DragEvent) {
+        e.preventDefault();
+        
+        const svgEl = e.target as SVGElement;
+        const { left, top } = svgEl.getBoundingClientRect();
+        const x = +(e.clientX - left).toFixed();
+        const y = +(e.clientY - top).toFixed();
+
+        const codeBlock = {
+			id: window.crypto.randomUUID(),
+			x: x,
+			y: y,
+		}
+
+        codeBlocks.push(codeBlock)
+        selected = codeBlock
+
+
+        // let data = e.dataTransfer.getData("Text");
+        // e.target.appendChild(document.getElementById(data));
+        // e.preventDefault();
+        // console.log("drop");
+    }
+
+    
+</script>
+
+<div class="container">
+    <div class="canvas">
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <svg viewBox="0 0 600 400" id="div1" ondrop={drop} ondragover={allowDrop}>
+            {#each codeBlocks as codeBlock}
+                <rect {...codeBlock} fill={"#555"} onclick={(e) => {
+                    e.stopPropagation()
+                }} oncontextmenu={(e) => {
+                    e.preventDefault()
+                }}></rect>
+            {/each}
+        </svg>
+    </div>
+    <div class="blocks">
+        <p class="block" id="drag1" draggable="true">
+            Block
+        </p>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        gap: 1rem;
+    }
+
+    .canvas {
+        width: 100%;
+    }
+    .blocks {
+        width: 100%;
+        background-color: beige;
+
+        .block {
+            background-color: lightcoral;
+            color: white;
+            display: inline-block;
+            padding: 10px;
+            border-radius: 5px;
+        }
+
+        .block:hover {
+            cursor: move;
+        }
+    }
+
+    .canvas svg {
+        background-color: lightcyan;
+        height: 100vh;
+        width: 100%;
+    }
+</style>
